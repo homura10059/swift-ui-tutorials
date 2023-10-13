025b03f09ccae468f2c47e060c9828217370a546
homura <preasper0+github@gmail.com>
2023-10-13 19:51:29 +0900

    add

--- building-lists-and-navigation/Landmarks/Landmarks/Views/CircleImage.swift
+++ building-lists-and-navigation/Landmarks/Landmarks/Views/CircleImage.swift
@@ -1,23 +1,18 @@
-//
-//  CircleImage.swift
-//  Landmarks
-//
-//  Created by 大村勇人 on 2023/10/04.
-//
-
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(Circle())
-            .overlay{
-                      Circle().stroke(.white, lineWidth: 4)
-                  }
+            .overlay {
+            Circle().stroke(.white, lineWidth: 4)
+        }
             .shadow(radius: 7)
     }
 }
 
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
